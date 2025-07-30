6e5d46c9739be19476a74a1bf2d48b3230ada07b
Hangum Archana <hangumarchana9b@gmail.com>
2025-07-30 14:27:37 +0530

    updated webiste

---
+++ DataLoader.js
@@ -0,0 +1,15 @@
+let database;
+async function dataLoader(){
+    try{
+        const response = await fetch('Data.json');
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        database = await  response.json();
+    }
+    catch (error){
+        console.log(error);
+    }
+
+
+}
