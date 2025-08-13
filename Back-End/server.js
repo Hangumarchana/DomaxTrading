965f74e05f78ec1454def8d664dda464601bbf5e
Hangum Archana <hangumarchana9b@gmail.com>
2025-08-13 14:00:15 +0530

    update

--- Back-End/server.js
+++ Back-End/server.js
@@ -16,12 +16,15 @@ app.post('/send', async (req, res) => {
     const { firstName, lastName, email, subject, message } = req.body;
 
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
         }
     });
+
 
     const mailOptions = {
         from: email,
